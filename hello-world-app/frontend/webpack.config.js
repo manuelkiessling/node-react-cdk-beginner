900b81fc5646b6972a37b79be6050d498b17ce0b
Manuel Kießling <manuel@kiessling.net>
2019-11-01 15:18:09 +0100

    Removed webpack HMR plugin.

--- hello-world-app/frontend/webpack.config.js
+++ hello-world-app/frontend/webpack.config.js
@@ -28,6 +28,5 @@ module.exports = {
         contentBase: path.join(__dirname, 'public/'),
         port: 3000,
         publicPath: 'http://localhost:3000/build/'
-    },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+    }
 };
